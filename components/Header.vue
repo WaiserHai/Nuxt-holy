63d9cdc31b3df7fc813fca80e31e3458a1e749b0
Noah Röschard <Noah@roeschard.eu>
2022-12-01 23:35:00 +0100

    test 2

--- components/Header.vue
+++ components/Header.vue
@@ -9,7 +9,9 @@
             <NuxtLink to="/">Site name here</NuxtLink>
           </div>
           <ul>
-            <li class="flex flex-col gap-5 mb-5 md:mb-0 mt-5 md:flex-row md:items-center md:justify-end sm:mt-0 md:pl-5">
+            <li
+              class="flex flex-col gap-5 mb-5 md:mb-0 mt-5 md:flex-row md:items-center md:justify-end sm:mt-0 md:pl-5"
+            >
               <NuxtLink class="navElement" to="/">Home</NuxtLink>
               <NuxtLink class="navElement" to="/about">About</NuxtLink>
               <NuxtLink class="navElement" to="/1-test">1-test</NuxtLink>
@@ -22,12 +24,11 @@
 </template>
 
 <style scoped>
-
 .navElement:not([aria-current="page"]) {
-    @apply font-bold block py-2 pr-4 pl-3 dark:text-gray-400 text-gray-700 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md:dark:hover:text-gray-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent;
+  @apply font-bold block py-2 pr-4 pl-3 dark:text-gray-400 text-gray-700 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md:dark:hover:text-gray-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent;
 }
 
 .navElement[aria-current="page"] {
-    @apply font-bold block py-2 pr-4 pl-3 text-black rounded md:bg-transparent text-blue-500 md:p-0 dark:text-black
+  @apply font-bold block py-2 pr-4 pl-3 text-black rounded md:bg-transparent text-blue-500 md:p-0 dark:text-black;
 }
 </style>
